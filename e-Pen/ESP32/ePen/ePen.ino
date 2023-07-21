8515535c9bad536ea31e6ac152d8a9cf3ca078a6
Jackson Majewski <jacksonmajewski@gmail.com>
2023-07-21 04:25:43 -0400

    Added IP display support

--- e-Pen/ESP32/ePen/ePen.ino
+++ e-Pen/ESP32/ePen/ePen.ino
@@ -1,6 +1,6 @@
 /*
  * Written by Jackson Majewski for Arkhe
- * Lots of server code shamelessly """borrowed""" from Rui Santos (http://randomnerdtutorials.com)
+ * Some server code shamelessly """borrowed""" from Rui Santos (http://randomnerdtutorials.com)
  */
 
 /* Includes ------------------------------------------------------------------*/
@@ -19,10 +19,9 @@
 const char *ssid = "apeiron";
 const char *pwrd = "enantiodromia..";
 WiFiServer server(80);
-UBYTE *BlackImage, *RYImage;
 unsigned long currentTime = millis();
 unsigned long previousTime = 0;
-const long timeoutTime = 10000;
+const long timeoutTime = 10000; //ms
 unsigned char* imgBuffer;
 bool shouldShutDown = false;
 
@@ -42,34 +41,13 @@ void setup()
   Serial.println("====================================");
   DEV_Module_Init();
 
-  Serial.println("Allocating on heap...");
+  Serial.println("Allocating image buffer...");
   imgBuffer = new unsigned char[ARR_SIZE]{0xFF};
 
   Serial.println("e-Paper Init and Clear...");
   EPD_7IN5B_V2_Init();
   EPD_7IN5B_V2_Clear();
-  DEV_Delay_ms(500);
-
-  // Create a new image cache named IMAGE_BW and fill it with white
-  UWORD Imagesize = ((EPD_7IN5B_V2_WIDTH % 8 == 0) ? (EPD_7IN5B_V2_WIDTH / 8) : (EPD_7IN5B_V2_WIDTH / 8 + 1)) * EPD_7IN5B_V2_HEIGHT;
-  if ((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL)
-  {
-    Serial.println("Failed to apply for black memory...");
-    while (1)
-      ;
-  }
-  if ((RYImage = (UBYTE *)malloc(Imagesize)) == NULL)
-  {
-    Serial.println("Failed to apply for red memory...");
-    while (1)
-      ;
-  }
-
-  // Select Image
-  Paint_SelectImage(BlackImage);
-  Paint_Clear(WHITE);
-  Paint_SelectImage(RYImage);
-  Paint_Clear(WHITE);
+  DEV_Delay_ms(500);  
 
   // show splash screen
   Serial.println("Displaying splash screen...");
@@ -81,13 +59,15 @@ void setup()
   // Attempt to connect to WiFi
   Serial.print("Attempting to connect to \"");
   Serial.print(ssid);
-  Serial.println("...");
+  Serial.println("\"...");
 
   WiFi.begin(ssid, pwrd);
   while (WiFi.status() != WL_CONNECTED)
   {
+    digitalWrite(LED_BUILTIN, HIGH); // Flashes light while awaiting connection (in theory)
     delay(500);
     Serial.print(".");
+    digitalWrite(LED_BUILTIN, LOW);
   }
   
   // Print local IP address and start web server
@@ -95,6 +75,17 @@ void setup()
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+  // Show WiFi address on screen
+  EPD_7IN5B_V2_Init();
+  Paint_NewImage(imgBuffer, EPD_7IN5B_V2_WIDTH, EPD_7IN5B_V2_HEIGHT, 0, WHITE);
+  Paint_SelectImage(imgBuffer);
+  Paint_Clear(WHITE);
+  Paint_DrawString_EN(10, 0, WiFi.localIP().toString().c_str(), &Font24, WHITE, BLACK);
+  EPD_7IN5B_V2_Display(imgBuffer, whiteBuffer);
+  Paint_Clear(WHITE);
+  EPD_7IN5B_V2_Sleep();
+
   server.begin();
 }
 
@@ -118,7 +109,7 @@ void loop()
       currentTime = millis();
       if (client.available())
       {
-        digitalWrite(LED_BUILTIN, HIGH); // Turn on LED when client connects
+        digitalWrite(LED_BUILTIN, HIGH); // Turn on LED while client is transmitting data
         imgBuffer[index] = client.read();
 
         // Check if pure white
@@ -135,14 +126,12 @@ void loop()
     Serial.println("Client disconnected.");
     digitalWrite(LED_BUILTIN, LOW);
 
-
-    if(shouldShutDown == true)
-    {
-      exit();
-    }
     if (index == ARR_SIZE)
     {
-      //Serial.println("Size match!");
+      if(shouldShutDown == true)
+      {
+        exit();
+      }
       Serial.println("Drawing received data!");
       EPD_7IN5B_V2_Init();
       EPD_7IN5B_V2_Clear();
@@ -156,23 +145,18 @@ void loop()
 /* Exit func ------------------------------------------------------------------*/
 void exit()
 {
-  Serial.println("Exiting...");
-
-  Serial.println("Init...");
+  Serial.println("Preparing for shutdown...");
   EPD_7IN5B_V2_Init();
 
-  Serial.println("Clear...");
+  Serial.println("Clearing display...");
   EPD_7IN5B_V2_Clear();
 
-  Serial.println("Goto Sleep...");
+  Serial.println("Putting display to Sleep...");
   EPD_7IN5B_V2_Sleep();
-  free(BlackImage);
-  free(RYImage);
-  BlackImage = NULL;
-  RYImage = NULL;
 
   Serial.println("Deleting imgBuffer...");
   delete[] imgBuffer;
+  imgBuffer = NULL;
   Serial.println("Goodbye!");
 
   esp_deep_sleep_start();
