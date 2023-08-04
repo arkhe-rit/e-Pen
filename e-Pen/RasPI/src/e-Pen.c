3867c880dd1c50015db73a3b04a7c0f84a037c57
Jackson Majewski <jacksonmajewski@gmail.com>
2023-08-04 04:41:20 -0400

    Redis connection working

--- e-Pen/RasPI/src/e-Pen.c
+++ e-Pen/RasPI/src/e-Pen.c
@@ -10,32 +10,49 @@
 #define WHITE 0xFF
 #define BLACK 0x00
 
-#define REDIS_DEFAULT_IP "127.0.0.1";
-#define REDIS_DEFAULT_PORT 6379;
+#define REDIS_DEFAULT_IP "172.20.10.3"
+#define REDIS_DEFAULT_PORT 6379
 
-#define REDIS_SUB_CMD "SUBSCRIBE projector/eink"
+#define REDIS_SUB_CMD "SUBSCRIBE channel projector/eink"
 
-const char* redisIP = "127.0.0.1";
-int redisPort = 6379;
+enum messageProperty = {type, command, value};
 
-void onMessage(redisAsyncContext *c, void *reply, void *privdata) {
-    printf("Received message!\r\n");
+void onMessage(const redisAsyncContext *c, void *reply, void *privdata)
+{
     redisReply *r = reply;
-    if (reply == NULL) {
-        printf("Message is NULL :(\r\n");
+    if (reply == NULL)
+    {
         return;
     }
-
-    printf("Message is not NULL :)\r\n");
-    if (r->type == REDIS_REPLY_STRING){
-        printf("String received: %s\r\n", r->str);
-    }
-
-    if (r->type == REDIS_REPLY_ARRAY) {
-        for (int j = 0; j < r->elements; j++) {
+    
+    if (r->type == REDIS_REPLY_ARRAY)
+    {
+        printf("Array Received: \r\n")
+        for (int j = 0; j < r->elements; j++)
+        {
             printf("%u) %s\n", j, r->element[j]->str);
         }
+
     }
+}
+
+int onConnect(const redisAsyncContext *c, int status)
+{
+    if(status != REDIS_OK){
+        printf("ERROR: %s\r\n", c->errstr);
+        return 1;
+    }
+    printf("Connected successfully!\r\n");
+    return 0;
+}
+
+void onDisconnect(const redisAsyncContext *c, int status)
+{
+    if(status != REDIS_OK){
+        printf("ERROR: %s\r\n", c->errstr);
+        return;
+    }
+    printf("Disconnected successfully!\r\n");
 }
 
 void loadAndDrawBMP(UBYTE *blackImgCache, const char *blackBMPPath, UBYTE *redImgCache, const char *redBMPPath)
@@ -83,37 +100,15 @@ void exitDisplay(UBYTE *blackImage, UBYTE *redImage)
     DEV_Module_Exit();
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     // UNIX moment
-    if(geteuid())
+    signal(SIGPIPE, SIG_IGN);
+    if (geteuid())
     {
         printf("ERROR: Please run this program as root.\r\n");
         return 1;
     }
-
-    /*
-    int bufsize = 16;
-    char* redisIP = malloc(bufsize * sizeof(char));
-    int redisPort;
-
-    // Handle commandline args
-    if(argc > 2){
-        for(unsigned int i = 0; i < argc; i++){
-            switch()
-        }
-    }
-    else{
-        redisIP = REDIS_DEFAULT_IP;
-        redisPort = REDIS_DEFAULT_PORT;
-    }
-
-    printf("Redis IP: %s\r\n", redisIP);
-    int i = 15553; // I'm just checking for an overflow
-    i++;
-    printf("Redis Port: %s\r\n", redisPort);
-    sleep(10); // DEBUG for sure
-    */
 
     printf("Arkhe e-Pen for Waveshare 7.5\" (B)\r\n");
 
@@ -156,40 +151,39 @@ int main(int argc, char* argv[])
     GUI_ReadBmp("./imgs/ArkheLogo_r.bmp", 0, 0);
 
     EPD_7IN5B_V2_Display(blackImg, redImg);
+    
     EPD_7IN5B_V2_Sleep(); // This is all an evil trick to ensure the e-Paper is in sleep mode for the loop >:)
 
-    // Create connect to redis (thanks to https://github.com/redis/hiredis/issues/55#issuecomment-4269731)
+    // Create connection to Redis
     printf("Attempting to connect to Redis...\r\n");
-    struct event_base *base = event_base_new();
+    const struct event_base *base = event_base_new();
 
-    redisAsyncContext *c = redisAsyncConnect(redisIP, redisPort);
-    if (c->err){
+    redisAsyncContext *c = redisAsyncConnect(REDIS_DEFAULT_IP, REDIS_DEFAULT_PORT);
+    if (c->err)
+    {
         printf("Error: %s\n", c->errstr);
+        // DC & quit
         redisAsyncFree(c);
         c = NULL;
+        exitDisplay(blackImg, redImg);
         return 1;
-    }
-    printf("Success!\r\n");
-    if(redisLibeventAttach(c, base) == REDIS_OK){
-        printf("Attached to libevent!\r\n");
-    }
-    else{
-        printf("ERROR: Unable to attach libevent!\r\n");
-        return 1;
-    }
-    redisAsyncCommand(c, onMessage, NULL, REDIS_SUB_CMD);
-    printf("Ran ");
-    printf(REDIS_SUB_CMD);
-    printf("\r\n");
-    while(1){ // DEBUG
-    event_base_dispatch(base);
     }
 
-    printf("Initializing exit sequence...\r\n"); // DEBUG
-    exitDisplay(blackImg, redImg);
+    redisLibeventAttach(c, base);
+    printf("Attached to event!\r\n");
+
+    redisAsyncSetConnectCallback(c, onConnect);
+    redisAsyncSetDisconnectCallback(c, onDisconnect);
+    printf("Set callbacks!\r\n");
+
+    redisAsyncCommand(c, onMessage, NULL, REDIS_SUB_CMD);
+    event_base_dispatch(base);
 
     printf("Disconnecting from Redis...\r\n");
     redisAsyncFree(c);
+
+    printf("Beginning to exit...\r\n");
+    exitDisplay(blackImg, redImg);
 
     return 0;
 }
